1781cccb3a0a91c7ade66ff72674b86cca16ebf9
Biplov Karna <57299583+biplovkarna017@users.noreply.github.com>
2023-02-01 03:31:34 +0545

    Update index.js
    Cleaned unnecessary comments

--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,12 @@
 const express = require("express");
-// const pdfroute = require("./routes/pdf")
-const corsHeaders = require("./middleware/cors")
+const corsHeaders = require("./middleware/cors");
 const fileupload = require("express-fileupload");
-// const bodyParser = require('body-parser');
-// const mammoth = require("mammoth");
-// const puppeteer = require("puppeteer");
-// const libre = require("libreoffice-convert");
-// const { convert } = require("libreoffice-convert");
-const path = require('path');
-const fs = require("fs");
-const app = express();
-app.options("*",corsHeaders);
-app.use(corsHeaders);
-// app.use(express.json());
-// app.use("/api/pdf", pdfroute)
-
-app.use(fileupload());
-// app.use(express.static("files"));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 const { spawn } = require("child_process");
+const path = require("path");
+const app = express();
+app.options("*", corsHeaders);
+app.use(corsHeaders);
+app.use(fileupload());
 
 const convertToPdf = (newpath) => {
   return new Promise((resolve, reject) => {
@@ -31,7 +18,7 @@ const convertToPdf = (newpath) => {
       "--outdir",
       "public",
     ]);
-    process.on('close', (code) => {
+    process.on("close", (code) => {
       if (code === 0) {
         resolve();
       } else {
@@ -42,36 +29,31 @@ const convertToPdf = (newpath) => {
 };
 
 app.post("/upload", async (req, res) => {
-    
-   
   const file = req.files.file;
   const filename = file.name;
-  const newpath = path.join(__dirname,"public",filename);
+  const newpath = path.join(__dirname, "public", filename);
 
   await file.mv(newpath, async (err) => {
     if (err) {
       res.status(500).send({ message: "File upload failed", code: 200 });
     }
     convertToPdf(newpath)
-    .then(() => {
-      res.status(200).send({ message: "File Uploaded", code: 200 });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send({ message: "File Upload Failed", code: 500 });
-    });
+      .then(() => {
+        res.status(200).send({ message: "File Uploaded", code: 200 });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send({ message: "File Upload Failed", code: 500 });
+      });
   });
-  
 });
 
 app.use(express.static("public"));
 
 app.get("/download/:fileName", (req, res) => {
-
   const fileName = req.params.fileName;
-  const newFile = path.parse(fileName).name + '.pdf';
-  const filePath = path.join(__dirname,"public", newFile);
-  console.log(filePath);
+  const newFile = path.parse(fileName).name + ".pdf";
+  const filePath = path.join(__dirname, "public", newFile);
   res.download(filePath, (err) => {
     if (err) {
       console.error("Error downloading file: ", err);
@@ -80,5 +62,5 @@ app.get("/download/:fileName", (req, res) => {
   });
 });
 
-app. listen (8080,()=> console.log("server is running" ));
+app.listen(8080, () => console.log("server is running"));
 module.export = app;
